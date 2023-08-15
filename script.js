01bf562f8983bacc124b125c127cde48ff8744bf
Mtlightfoot <M-lightfoot@hotmail.com>
2023-08-15 11:59:17 +0100

    Started Etch-a-Sketch project, created a div container to hold the 16x16 grid using javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+const container = document.createElement("div");
+container.id = "container";
+container.className = "container";
+container.style = "background-color: red;";
+container.style.width = "100%";
+container.style.height = "600px";
+container.style.display = "flex";
+container.style.marginLeft = "auto";
+container.style.marginRight = "auto";
+container.style.justifyContent = "center";
+document.body.appendChild(container);
+
