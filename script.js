a6714d90f07840e1ce2a96bb5c0bfafa276543cd
Mtlightfoot <M-lightfoot@hotmail.com>
2023-08-15 16:31:12 +0100

    Added amount of pixels button, created mouseover and mouseout event listeners on it

--- script.js
+++ script.js
@@ -3,32 +3,50 @@ console.log("hello");
 const container = document.createElement("div");
 container.id = "container";
 container.className = "container";
-container.style.maxWidth = "500px";
-container.style.maxHeight = "500px";
+container.style.maxWidth = "600px";
+container.style.maxHeight = "600px";
 container.style.margin = "0 auto";
-container.style.border = "red 2px solid"
 document.body.appendChild(container);
 
-let pixel = 100;
-
-for (let i = 0; i < pixel; i++) {
-    let row = document.createElement("div");
-    row.className = "row";
-    row.style.display = "flex";
-    for (let j = 0; j < pixel; j++) {
-        let box = document.createElement("div");
-        box.className = "box";
-        box.style.flex = "1";
-        box.style.backgroundColor = "white";
-        box.style.border = "black 1px solid";
-        box.style.aspectRatio = "1 / 1";
-        row.appendChild(box);
-        box.addEventListener("mouseover", function (e) {
-            e.target.style.backgroundColor = "black";
-        })
-        document.getElementById("container").appendChild(row);
-
+function createGrid(pixel) {
+    for (let i = 0; i < pixel; i++) {
+        let row = document.createElement("div");
+        row.className = "row";
+        row.style.display = "flex";
+        for (let j = 0; j < pixel; j++) {
+            let box = document.createElement("div");
+            box.className = "box";
+            box.style.flex = "1";
+            box.style.backgroundColor = "white";
+            box.style.border = "black 1px solid";
+            box.style.aspectRatio = "1 / 1";
+            row.appendChild(box);
+            box.addEventListener("mouseover", function (e) {
+                e.target.style.backgroundColor = "black";
+            })
+            document.getElementById("container").appendChild(row);
+        }
     }
-}
+};
 
+const button = document.createElement("button");
+button.id = "button";
+button.className = "button";
+button.textContent = "Amount of pixels";
+button.style.border = "3px solid black"
+button.style.borderRadius = "5px";
+button.style.marginTop = "25px";
+button.style.width = "250px";
+button.style.height = "50px";
+button.style.backgroundColor = "light gray";
+button.style.boxShadow = "0px 2px 3px grey"
+button.addEventListener("mouseover", function(e) {
+    e.target.style.backgroundColor = "white"
+});
+button.addEventListener("mouseout", function(e) {
+    e.target.style.backgroundColor = "";
+});
+document.body.appendChild(button);
+
+createGrid(100);
 
