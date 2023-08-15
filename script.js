3ee0854c008fef0bf793dfd012fe284353d9b67c
Mtlightfoot <M-lightfoot@hotmail.com>
2023-08-15 15:04:59 +0100

    Created a hover option to make the grid color in as you mouseover the boxes

--- script.js
+++ script.js
@@ -3,12 +3,31 @@ console.log("hello");
 const container = document.createElement("div");
 container.id = "container";
 container.className = "container";
-container.style = "background-color: red;";
-container.style.width = "100%";
-container.style.height = "600px";
+container.style.maxWidth = "100%";
 container.style.display = "flex";
 container.style.marginLeft = "auto";
 container.style.marginRight = "auto";
 container.style.justifyContent = "center";
 document.body.appendChild(container);
+
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.className = "row";
+    row.style.margin = "0";
+    for (let j = 0; j < 16; j++) {
+        let box = document.createElement("div");
+        box.className = "box";
+        box.style.backgroundColor = "white";
+        box.style.width = "40px";
+        box.style.height = "40px";
+        box.style.margin = "0";
+        box.style.border = "black 1px solid"
+        row.appendChild(box);
+        box.addEventListener("mouseover", function (e) {
+            e.target.style.backgroundColor = "black";
+        })
+        document.getElementById("container").appendChild(row);
+
+    }
+}
 
